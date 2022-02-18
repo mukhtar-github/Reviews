c7485cb880c55bea315324752a04c783a1ad08e7
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-18 10:14:56 +0100

    Update file

--- reviews-app/src/Review.js
+++ reviews-app/src/Review.js
@@ -24,7 +24,45 @@ const nextPerson = () => {
   });
 };
 
-  return <h2>review component</h2>;
+const prevPerson = () => {
+  setIndex((index) => {
+    let newIndex = index - 1;
+    return checkNumber(newIndex);
+  });
+};
+
+const randomPerson = () => {
+  let randomNumber = Math.floor(Math.random() * people.length);
+  if (randomNumber === index) {
+    randomNumber = index + 1;
+  }
+  setIndex(checkNumber(randomNumber));
+};
+
+return (
+  <article className='review'>
+    <div className='img-container'>
+      <img src={image} alt={name} className='person-img' />
+      <span className='quote-icon'>
+        <FaQuoteRight />
+      </span>
+    </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
+    <div className='button-container'>
+      <button className='prev-btn' onClick={prevPerson}>
+        <FaChevronLeft />
+      </button>
+      <button className='next-btn' onClick={nextPerson}>
+        <FaChevronRight />
+      </button>
+    </div>
+    <button className='random-btn' onClick={randomPerson}>
+      suprise me
+    </button>
+  </article>
+);
 };
 
 export default Review;
