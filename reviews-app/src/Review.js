ce43c89b21cdfb7c7f8eea6bf208ab261ebdcf15
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-18 07:58:50 +0100

    Update files

--- reviews-app/src/Review.js
+++ reviews-app/src/Review.js
@@ -3,6 +3,27 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
+
+const [index, setIndex] = useState(0);
+const { name, job, image, text } = people[index];
+
+const checkNumber = (number) => {
+  if (number > people.length - 1) {
+    return 0;
+  }
+  if (number < 0) {
+    return people.length - 1;
+  }
+  return number;
+};
+
+const nextPerson = () => {
+  setIndex((index) => {
+    let newIndex = index + 1;
+    return checkNumber(newIndex);
+  });
+};
+
   return <h2>review component</h2>;
 };
 
